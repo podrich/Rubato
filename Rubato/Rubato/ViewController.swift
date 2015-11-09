eb7c803908f337f94319da3276988628b43e484c
Peter Washington <peter.y.washington@gmail.com>
2015-11-08 18:14:02 -0800

    Rubato so far

--- Rubato/Rubato/ViewController.swift
+++ Rubato/Rubato/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
+    
 
 }
 
